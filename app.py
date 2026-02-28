bd6618bff073bcda820bee9f3077a6f3c42bb22c
Regita Juairiani <regitaaajuairianiii@gmail.com>
2026-02-28 09:17:09 +0700

    Add Plotly imports for data visualization

--- app.py
+++ app.py
@@ -5,6 +5,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 
